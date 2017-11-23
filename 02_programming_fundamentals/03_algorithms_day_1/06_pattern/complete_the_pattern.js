5bec6ccb7befba0fe584341056001226daed63c9
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-23 18:48:41 +0100

    did exercise 06 on patterns

--- 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
+++ 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
@@ -1,3 +1,25 @@
 // Modify this value to test with other values
-const numberOfLine = 5;
+const numberOfLine = 11;
 // Your code here ⬇
+
+
+//54321 i = 5 et j = 5 à 1
+//5432  i = 4 et j = 5 à 2
+//543
+//54
+//5
+
+
+
+let stringout = "";
+
+for (let i=numberOfLine; i>0;i--) {
+
+  for (let j=numberOfLine; j>numberOfLine-i; j--) {
+    stringout = stringout + j;
+  }
+
+  stringout = stringout + "\n";
+}
+
+console.log(stringout);
