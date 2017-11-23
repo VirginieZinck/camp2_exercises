e2455bc80bfb365c1122fdcc7fb263d9d9712383
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-23 13:35:43 +0100

    exercices sur les arrays en cours

--- 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
+++ 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
@@ -3,10 +3,11 @@
  */
 
 // Rewrite your array `digits` using `push`;
-
+let digits = [];
+for (let i=0;i<10;i++) {digits.push(i);}
 
 // rewrite your variable `last` using `length`;
-
+const last = digits[digits.length-1];
 
 // create another array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 
