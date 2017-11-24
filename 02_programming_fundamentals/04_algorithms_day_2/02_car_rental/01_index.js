6b6c7f0db9e668235471d85ce4253d6a33b2c1ee
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-24 15:01:44 +0100

    did exercise 02_car_rental

--- 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
@@ -1,5 +1,5 @@
 const driver = {
-  driverLicense: "B1",
+  driverLicense: "D",
   licenseIssued: new Date().getFullYear() - 10, // 10 years old license
   numberOfAccident: 0,
   bonus: 0.8,
@@ -9,7 +9,22 @@ const driver = {
 // * Input: a driver
 // * Output: a boolean if the driver can rent a car
 
+function canRentACar(aDriver) {
+  let canRentACar;
+  if ((aDriver.driverLicense === "B" || aDriver.driverLicense === "B1") &&
+      aDriver.licenseIssued <= 2015 &&
+      (aDriver.numberOfAccident===0 || aDriver.bonus >= 0.7 )) {
+    canRentACar = true;
+  } else {
+    canRentACar = false;
+  }
+  console.log(aDriver);
+  console.log(canRentACar);
+  return canRentACar;
+}
 
+
+canRentACar(driver);
 
 
 // ⚠ Do not remove me ! It's for tests
