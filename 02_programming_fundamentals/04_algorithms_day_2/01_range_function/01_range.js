f38837f6ffd331e8bfde6c18dba25fd8ef1118dd
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-24 14:29:25 +0100

    exercise 01-range done

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,9 +4,31 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
 
+
+function range(start,end) {
+
+  const resultArray=[];
+  const arraySize = Math.abs(end - start) + 1;
+
+  for (let i=0 ; i<arraySize ; i++) {
+
+    if (start<end) {
+      resultArray.push(start+i);
+    } else {
+      resultArray.push(start-i);
+    }
+    
+  }
+  console.log(resultArray);
+  return resultArray;
 }
+
+
+range(7,2);
+
+range(3,6);
+
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
