755bec16422208f7d97e1dbc4f252bdfbb65ad75
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-24 16:08:09 +0100

    did exercise 01-filter-with-string

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,8 +1,34 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
+
+
 function filter(array, str) {
-  // Your code here
+
+  const resultArray = [];
+  let isEven;
+
+  if (str !=="even" && str !=="odd") {
+    console.log("vous avez passé un mauvais paramètre : "+str);
+    return resultArray;
+  }
+
+  for (let i=0; i<array.length;i++) {
+
+    isEven=(array[i]%2 === 0);
+
+    if (str ==="even" && isEven===true) {
+      resultArray.push(array[i]);
+    } else if (str ==="odd" && isEven===false) {
+      resultArray.push(array[i]);
+    }
+  }
+
+  return resultArray;
 }
+
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15,16];
+const returnTestArray = filter(testArray,"blabla");
+console.log(returnTestArray);
 
 // do not remove this line, it is for tests
 module.exports = filter;
