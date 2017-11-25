e638174ba571b668600943c98af64b84f2cf1899
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-25 10:18:20 +0100

    did exercise 02-filter-with-function

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
@@ -1,8 +1,46 @@
 // filter takes an array of integer and a function of filtering
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
+
+
+function pickEvenNumbers(value) {
+  let isEven = (value%2 === 0);
+  return isEven;
+}
+
+function pickOddNumbers(value) {
+  let isOdd = (value%2 !== 0);
+  return isOdd;
+}
+
 function filter(array, fn) {
   // Your code here
+  const resultArray = [];
+
+  if (fn !==pickEvenNumbers && fn !==pickOddNumbers) {
+    console.log("vous avez passé une fonction inconnue en paramètre : "+fn);
+    return resultArray;
+  }
+
+  for (let i=0; i<array.length;i++) {
+
+    if (fn === pickEvenNumbers) {
+      if (pickEvenNumbers(array[i])) {
+        resultArray.push(array[i]);
+      }
+    }
+    if (fn ===pickOddNumbers) {
+      if (pickOddNumbers(array[i])) {
+        resultArray.push(array[i]);
+      }
+    }
+  }
+
+  return resultArray;
 }
+
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15,16];
+const returnTestArray = filter(testArray,pickEvenNumbers);
+console.log(returnTestArray);
 
 // do not remove this line, it is for tests
 module.exports = filter;
