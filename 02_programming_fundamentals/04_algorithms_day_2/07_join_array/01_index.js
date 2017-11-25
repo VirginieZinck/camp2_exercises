5039c73fa2be6ab62eb2424ca47ffba0ec8d53f2
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-25 12:34:26 +0100

    did exercise 07 join array

--- 02_programming_fundamentals/04_algorithms_day_2/07_join_array/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/07_join_array/01_index.js
@@ -5,6 +5,25 @@
 // eg: join(["zero", "one", "two"], "-") => "zero - one - two"
 
 
+
+function joinArray(array,separator) {
+
+  function cumul(accum,value,index) {
+    if (index!==array.length-1) {
+      return accum+value+separator;
+    } else {
+      return accum+value;
+    }
+  }
+
+  return array.reduce(cumul,"");
+
+}
+
+console.log("[\"zero\", \"one\", \"two\"] =>" + joinArray(["zero", "one", "two"]," - "));
+console.log(joinArray(["A", "B", "C"], " ~ "));
+console.log("[] =>" + joinArray([]," - "));
+
 // ⚠ Do not remove me ! It's for tests
 // eslint-disable-next-line
 module.exports = joinArray;
