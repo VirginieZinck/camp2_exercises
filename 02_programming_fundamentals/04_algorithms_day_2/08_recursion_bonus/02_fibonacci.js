e10883e2da3ca402ced6a496137b14b002260029
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-27 11:47:33 +0100

    exercises 02_fibonacci en cours

--- 02_programming_fundamentals/04_algorithms_day_2/08_recursion_bonus/02_fibonacci.js
+++ 02_programming_fundamentals/04_algorithms_day_2/08_recursion_bonus/02_fibonacci.js
@@ -16,7 +16,24 @@
 
 function fibo(n) {
   // your code here
+
+  if (n>2 && n%1 ===0) {
+    return fibo(n-1)+fibo(n-2);
+  } else if (n<0 || isNaN(n) || n%1 !==0) {
+    return null;
+  } else if (n===2) {
+    return 1;
+  } else if (n===1) {
+    return 0;
+  } else if (n===0) {
+    return null;
+  }
+  let result = fibo(n-1)+fibo(n-2)
 }
 
 // do not remove this line, it is for tests
 module.exports = fibo;
+
+console.log("fibo 5 => 3" + fibo(5)); //0,1,1,2,3 =>3
+console.log("fibo 4 => 2" + fibo(4)); //0,1,1,2 => 2
+console.log("fibo 3 => 1" + fibo(4)); //0,1,1 => 1
