606ae92247657ebe1d5002b2dc6750b73b997e2d
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-27 18:28:16 +0100

    did exercise sort an array

--- 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
+++ 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
@@ -8,7 +8,53 @@
 
 function sort(unsortedArray) {
   // Your code here
+
+  let sortedArray = [];
+  let didInsert;
+
+  for (let i =0;i<unsortedArray.length;i++) {
+
+    didInsert = false;
+
+    if (i===0) {
+      sortedArray.push(unsortedArray[i]);
+      console.log("1st index, I do a push");
+      console.log("unsortedArray"+unsortedArray);
+      console.log("sortedArray"+sortedArray);
+
+    } else {
+
+      let j=0;
+
+      while (didInsert===false && j<sortedArray.length) {
+
+        if (unsortedArray[i]<sortedArray[j]) {
+          //la valeur est plus petite que la valeur de l'index actuel
+          sortedArray.splice(j,0,unsortedArray[i]);
+          didInsert = true;
+          console.log("value is smaller than current index : I do a splice");
+          console.log("unsortedArray"+unsortedArray);
+          console.log("sortedArray"+sortedArray);
+
+        }
+        j++;
+      }
+
+      if (didInsert === false) {
+        sortedArray.push(unsortedArray[i]);
+        console.log("value is greater than last index : I do a push");
+        console.log("unsortedArray"+unsortedArray);
+        console.log("sortedArray"+sortedArray);
+
+      }
+    }
+  }
+  return sortedArray;
 }
+
+
+sort([24, 7, 9, 72, -8]);
+
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
