b4e09ec707476e7dd66c132e0a54ed0911841bda
Virginie Zinck <virginie.zinck@decathlon.com>
2017-11-28 17:00:59 +0100

    did memory exercise

--- 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
+++ 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
@@ -1,2 +1,81 @@
 // This function will clear the terminal when called
 const clear = require("cli-clear");
+const readline = require("readline");
+
+const cards = ["🌲","🐰", "🎃", "🎃", "🐰", "🌲"];
+let returnedCards = ["1","2","3","4","5","6"];
+
+let firstCard;
+let firstIndex;
+let secondCard;
+let secondIndex;
+
+let mixedCards = [];
+
+
+
+function mixCards(cards) {
+  let randomIndex;
+  for (let i=0;i<cards.length;i++) {
+    randomIndex = Math.floor(Math.random()*6);
+    mixedCards.splice(randomIndex,0,cards[i]);
+  }
+  console.log(mixedCards);
+}
+
+
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function newTry() {
+
+  clear();
+  console.log(returnedCards);
+
+  reader.question("Give nb of 1st card you want to return=>", (input) => {
+
+    firstIndex = returnedCards.indexOf(input);
+
+    if (firstIndex === -1) {
+      console.log(`invalid input ${input}`);
+      reader.question("Ready ? if yes, type any key", newTry);
+    } else {
+      firstCard = mixedCards[firstIndex];
+      console.log(`First card : ${firstCard}`);
+    }
+
+    reader.question("Give nb of 2nd card you want to return=>", (input) => {
+
+      secondIndex = returnedCards.indexOf(input);
+
+      if (secondIndex === -1 || secondIndex === firstIndex) {
+        console.log(`invalid input ${input}`);
+        reader.question("Ready ? if yes, type any key", newTry);
+      } else {
+        secondCard = mixedCards[secondIndex];
+        console.log(`Second card : ${secondCard}`);
+      }
+
+      if (firstCard === secondCard) {
+        console.log("You found a pair !");
+        returnedCards[firstIndex] = mixedCards[firstIndex];
+        returnedCards[secondIndex] = mixedCards[secondIndex];
+      }
+
+      if (returnedCards.join() === mixedCards.join()) {
+        console.log("Well done! You won!");
+        console.log(returnedCards);
+        reader.close();
+      } else {
+        reader.question("Ready ? if yes, type any key", newTry);
+      }
+
+    });
+  });
+}
+
+mixCards(cards);
+newTry();
