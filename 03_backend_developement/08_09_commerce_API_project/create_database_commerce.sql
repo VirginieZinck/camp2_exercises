95de43ca70c1aa6ec5be47f9a1c076f97a7b061d
Virginie Zinck <virginie.zinck@decathlon.com>
2017-12-13 18:43:44 +0100

    commerce API day 1

---
+++ 03_backend_developement/08_09_commerce_API_project/create_database_commerce.sql
@@ -0,0 +1,34 @@
+CREATE EXTENSION pgcrypto;
+
+create table categories (
+id 				uuid		PRIMARY KEY,
+decathlon_id    integer,
+label           varchar(255));
+
+drop table categories;
+
+delete from categories;
+
+create table brands (
+id 				uuid		PRIMARY KEY,
+title           varchar(255)
+);
+
+create table products (
+id 				uuid		PRIMARY KEY,
+decathlon_id    integer,
+title           varchar(255),
+description     text,
+brand_id        uuid        REFERENCES brands(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+min_price       float,
+max_price       float,
+crossed_price   float,
+percent_reduction float,
+image_path      varchar(255),
+rating          float
+);
+
+create table products_by_category (
+products_id     uuid 		PRIMARY KEY REFERENCES products(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+category_id     uuid        REFERENCES products(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
